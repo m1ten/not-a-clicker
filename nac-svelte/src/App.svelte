a2b08801a08a3fac5b926d27519fe4c97d05eadf
miten <57693631+m1ten@users.noreply.github.com>
2023-01-01 22:52:30 -0500

    nac

---
+++ nac-svelte/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+    let count = 0;
+    let value = 1;
+    function onClick() {
+        count += parseInt(value);
+    }
+</script>
+
+<div>
+    <input type="text" bind:value={value} />
+
+    <p>Count: {count}</p>
+
+    <button on:click={onClick}>Click me</button>
+</div>
